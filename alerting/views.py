1dde09f60c45a54e001b9b24e8e9085613693a61
mingming.tang <mingmingtang@aliyun.com>
2022-10-26 16:51:58 +0800

    202210261651

--- alerting/views.py
+++ alerting/views.py
@@ -8,9 +8,9 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import pagination
+from rest_framework.decorators import action
 
 from devops_django import permissions as dd_permissions
-
 
 from . import models as l_models
 from . import serializers as l_serializers
@@ -21,7 +21,6 @@ from prom.objects.alert import AlertObject
 from prom.objects.rule import RuleObject
 
 User = get_user_model()
-
 
 logger = logging.getLogger(__name__)
 
@@ -65,7 +64,7 @@ class Alert(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.IsAuthenticated,
                           dd_permissions.generate_user_obj_perm_class(user_filed="user", safe_methods=["OPTIONS"])]
 
-    def get_object(self):
+    def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user)
 
@@ -77,4 +76,20 @@ class Alert(viewsets.ReadOnlyModelViewSet):
     #     # headers = self.get_success_headers(serializer.data)
     #     return Response({}, 200)
 
+    @action(methods=["post"], detail=True, url_path="confirm")
+    def c_confirm(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.confirmed = True
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
+    @action(methods=["post"], detail=False, url_path="confirm-all")
+    def c_confirm_all(self, request, *args, **kwargs):
+        qs = self.get_queryset().filter(confirmed=False)
+        count = qs.count()
+        qs.update(confirmed=True)
+        data = {
+            "count": count
+        }
+        return Response(data)
