a592960e840e5cb2bf0f21310bda306c8dcc98fb
mingming.tang <mingmingtang@aliyun.com>
2022-10-26 10:43:28 +0800

    202210261043

--- data/views.py
+++ data/views.py
@@ -11,7 +11,7 @@ class Account(viewsets.ReadOnlyModelViewSet):
     serializer_class = l_serializers.Account
     queryset = l_models.Account.objects.all()
 
-    @action(methods=["get"], detail=False, url_path="account-balance-choices")
+    @action(methods=["get"], detail=False, url_path="account-balance-choices", permission_classes=[])
     def get_account_balance_choices(self, request, *args, **kwargs):
         account_qs = self.queryset.all()
         choices = [[item.public_key, f"{item.public_key} {item.ssv_balance_human} SSV"] for item in account_qs]
@@ -22,7 +22,7 @@ class Operator(viewsets.ReadOnlyModelViewSet):
     serializer_class = l_serializers.Operator
     queryset = l_models.Operator.objects.all()
 
-    @action(methods=["get"], detail=False, url_path="operator-choices")
+    @action(methods=["get"], detail=False, url_path="operator-choices", permission_classes=[])
     def get_operator_choices(self, request, *args, **kwargs):
         operator_qs = self.queryset.all()
         choices = [[item.id, f"{item.id} | {item.name}"] for item in operator_qs]
