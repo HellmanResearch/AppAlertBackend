5d4fcd59a90a2db8808a1f04e9b055729321a59f
mingming.tang <mingmingtang@aliyun.com>
2022-11-16 14:03:07 +0800

    202211161403

--- AppAlertBackend/settings_uat.py
+++ AppAlertBackend/settings_uat.py
@@ -1,7 +1,5 @@
 from .settings import *
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -14,3 +12,7 @@ DATABASES = {
 }
 
 CELERY_BROKER_URL = "amqp://alert:alert,1@localhost/alert",
+
+PROM_RULE_FILE = "/opt/prometheus-2.38.0.linux-amd64/rules/ssv_rules.yml"
+PROM_BASE_URL = "http://127.0.0.1:9090"
+PROM_ADDR = "127.0.0.1"
