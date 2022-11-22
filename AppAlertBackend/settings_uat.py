ee7abed94ee04bea5c97056d48f0757cd3f00430
mingming.tang <mingmingtang@aliyun.com>
2022-11-22 15:30:08 +0800

    202211221530

--- AppAlertBackend/settings_uat.py
+++ AppAlertBackend/settings_uat.py
@@ -14,5 +14,7 @@ DATABASES = {
 CELERY_BROKER_URL = "amqp://alert:alert,1@localhost/alert",
 
 PROM_RULE_FILE = "/opt/prometheus-2.38.0.linux-amd64/rules/ssv_rules.yml"
-PROM_BASE_URL = "http://39.101.77.40"
+PROM_BASE_URL = "http://127.0.0.1:9090"
 PROM_ADDR = "127.0.0.1"
+
+BASE_URL = "http://39.101.77.40:8000"
