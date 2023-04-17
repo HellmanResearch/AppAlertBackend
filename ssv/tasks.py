f6814b3454863f460b2f52f5fcc110ad2b30b0f9
mingming.tang <mingmingtang@aliyun.com>
2023-04-17 13:47:39 +0800

    202304171347

--- ssv/tasks.py
+++ ssv/tasks.py
@@ -9,6 +9,8 @@ from django.conf import settings
 
 from celery import shared_task
 from multiprocessing import Lock
+from websocket import create_connection
+
 
 from django.db.models import Max, Count, Min, Avg
 from websockets.sync.client import connect
@@ -79,34 +81,35 @@ def sync_decided():
     is_got = sync_decided_lock.acquire(timeout=1)
     if is_got is False:
         return
+    websocket = None
     try:
         logger.info("got lock")
-        with connect(f"{settings.SSV_NODE_WS}/stream") as websocket:
-            logger.info("connected")
-
-            for _ in range(2000):
-                message_str = websocket.recv()
-                message_body = json.loads(message_str)
-                for item in message_body["data"]:
-                    validator_public_key = "0x" + message_body["filter"]["publicKey"]
-                    height = item["Message"]["Height"]
-                    signers_str = ",".join([str(i) for i in item["Signers"]])
-
-                    try:
-                        l_models.Decided.objects.create(validator_public_key=validator_public_key,
-                                                        height=height,
-                                                        signers=signers_str)
-                    except IntegrityError as exc:
-                        decided = l_models.Decided.objects.get(validator_public_key=validator_public_key, height=height)
-                        decided.delete()
-                        l_models.Decided.objects.create(validator_public_key=validator_public_key,
-                                                        height=height,
-                                                        signers=signers_str)
+        websocket = create_connection(f"{settings.SSV_NODE_WS}/stream")
+        logger.info("connected")
+        while True:
+            message_str = websocket.recv()
+            message_body = json.loads(message_str)
+            for item in message_body["data"]:
+                validator_public_key = "0x" + message_body["filter"]["publicKey"]
+                height = item["Message"]["Height"]
+                signers_str = ",".join([str(i) for i in item["Signers"]])
+                try:
+                    l_models.Decided.objects.create(validator_public_key=validator_public_key,
+                                                    height=height,
+                                                    signers=signers_str)
+                except IntegrityError as exc:
+                    decided = l_models.Decided.objects.get(validator_public_key=validator_public_key, height=height)
+                    decided.delete()
+                    l_models.Decided.objects.create(validator_public_key=validator_public_key,
+                                                    height=height,
+                                                    signers=signers_str)
     except Exception as exc:
         sync_decided_lock.release()
         logger.warning(f"sync decided error exc: {exc}")
     else:
         sync_decided_lock.release()
+    if websocket is not None:
+        websocket.close()
 
 
 @shared_task
